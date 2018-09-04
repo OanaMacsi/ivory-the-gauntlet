03e9b169ac56fa55d9327cb9eba21af3c4609a45
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-09-04 13:04:24 +0300

    remove comments

--- linkedList.js
+++ linkedList.js
@@ -62,15 +62,6 @@ class LinkedList {
     currentNode = currentNode.next; 
   }
   return null;
-    //   let thisNode = this.head;
-    //   while (thisNode) {
-    //     if (thisNode.value === val) {
-    //       return thisNode;
-    //     }
-    //     thisNode = thisNode.next;
-    //   }
-    //   return thisNode;
-    // }
   }
 }
   
